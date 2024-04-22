10694ee70a7789936bb5c8f201ceb601bfc542ba
greywolf0324 <johngrey0324@gmail.com>
2024-04-21 22:10:55 -0400

    allow host

--- tablechatbot/settings.py
+++ tablechatbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3p4c01keng77g7sap4v&ajf+jf+qigx++w7om#_omlmmlz75+q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
