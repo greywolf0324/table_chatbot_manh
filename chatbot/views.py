37264bcd5dfbf94fe2447b871d789b113652e44d
greywolf0324 <johngrey0324@gmail.com>
2024-04-23 14:52:20 -0400

    add region to sagemakerendpoint reader

--- chatbot/views.py
+++ chatbot/views.py
@@ -103,7 +103,8 @@ def chatbot(request):
             client=boto3.client(
                 "runtime.sagemaker",
                 aws_access_key_id = aws_access_key_id,
-                aws_secret_access_key = aws_secret_access_key
+                aws_secret_access_key = aws_secret_access_key,
+                region = "us-east-2"
             )
         )
 
